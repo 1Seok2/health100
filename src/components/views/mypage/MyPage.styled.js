b804aa962dcbf3d9f1c5040dfba9091e1b0b7bf6
1seok2 <misohaha31@naver.com>
2020-09-28 00:20:07 +0900

    change some styles

--- src/components/views/mypage/MyPage.styled.js
+++ src/components/views/mypage/MyPage.styled.js
@@ -3,8 +3,8 @@ import {
   HEALTH_COLOR_DARK,
   HEALTH_COLOR,
   HEALTH_COLOR_LIGHT,
-  NEGATIVE_COLOR_LIGHT,
   NEGATIVE_COLOR,
+  NEGATIVE_COLOR_LIGHT,
 } from 'components/modules/style/Color';
 
 export const Container = styled.div`
@@ -54,13 +54,16 @@ export const STh = styled.th`
 export const STr = styled.tbody`
   margin: 0;
   height: 42px;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const Std = styled.td`
   margin: 0;
   border: 3px solid #ffffff;
   background-color: ${(props) =>
-    props.title ? NEGATIVE_COLOR_LIGHT : '#fafafa'};
+    props.title ? NEGATIVE_COLOR_LIGHT : '#efefef'};
   ${(props) =>
     props.current ? `background-color : ${HEALTH_COLOR_DARK};` : null}
   ${(props) => (props.current ? `color : white;` : null)}
@@ -76,6 +79,7 @@ export const ButtonWrapper = styled.div`
 `;
 
 export const SButton = styled.button`
+  float: right;
   width: 100px;
   height: 42px;
   padding: 12px;
@@ -84,8 +88,9 @@ export const SButton = styled.button`
   background-color: ${(props) =>
     props.bgColor ? NEGATIVE_COLOR : HEALTH_COLOR};
   color: white;
-  margin-right: 1.5rem;
+  margin-left: 1.5rem;
   &:hover {
+    cursor: pointer;
     background-color: ${(props) =>
       props.bgColor ? NEGATIVE_COLOR_LIGHT : HEALTH_COLOR_LIGHT};
   }
